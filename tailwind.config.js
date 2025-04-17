e20346dbb0fca21a136610bff587e060ab5fc09f
theo <tpetanid@live.com>
2025-04-17 22:51:41 +0200

    Add dark mode toggle functionality with Tailwind v4 custom variant

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./main/templates/**/*.html",
+    "./templates/**/*.html",
+  ],
+  darkMode: 'class', // Enable class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
